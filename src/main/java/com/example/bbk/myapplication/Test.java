e53868f091b331ff1baa85c9d1fe840c9fc241bc
iriri <1213468395@qq.com>
2018-10-18 10:19:38 +0800

    log ok

--- src/main/java/com/example/bbk/myapplication/Test.java
+++ src/main/java/com/example/bbk/myapplication/Test.java
@@ -1,9 +1,14 @@
 package com.example.bbk.myapplication;
 
+import android.util.Log;
+
 /**
  * Created by BBK on 2018/10/18.
  */
 
 public class Test {
     private static final String TAG = "Test";
+    public void log(){
+        Log.d(TAG, "log: ok");
+    }
 }
